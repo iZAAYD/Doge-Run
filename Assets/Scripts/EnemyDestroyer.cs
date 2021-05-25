2182fb0859e18f0942cbd476dc078bd3c090be16
Óscar Cruz <oscarcruz1411@gmail.com>
2021-05-25 12:10:02 -0600

    Clean Repo

--- Assets/Scripts/EnemyDestroyer.cs
+++ Assets/Scripts/EnemyDestroyer.cs
@@ -4,7 +4,6 @@ public class EnemyDestroyer : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("collision");
         if (other.gameObject.CompareTag("Enemy"))
         { 
             Destroy(other.gameObject);
